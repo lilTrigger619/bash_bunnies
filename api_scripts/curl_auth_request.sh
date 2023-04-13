1ba28adce2291ef2b8f65228a0134d5486169f9a
Shadrack Opoku <shaddyopoku1@gmail.com>
2023-04-13 19:48:54 +0000

    fix: solved issue that occurs when there host is not up.

--- api_scripts/curl_auth_request.sh
+++ api_scripts/curl_auth_request.sh
@@ -11,36 +11,43 @@ source "./utils.sh" # for all necessary functions
 refresh_token=$(getToken "./token.log" "refresh")
 access_token=$(getToken "./token.log" "access")
 request_url=$1
-# verify refresh token
-`verifyToken "$refresh_token" "/MainApp/verify/"`
-verify_response=$?
 
-if [[ $verify_response -ne 0 ]]
-then
-	echo "The refresh response is invalid. Please re-login!"
-fi
-unset verify_response
-
-# verify access toekn
-`verifyToken "$access_token" "/MainApp/verify/"`
-verify_response=$?
-
-if [[ $verify_response -ne 0 ]]
-then echo "Please refresh the token!";exit 1;
-fi
-
-
-$(checkFileNotEmpty "./token.log")
-
+# check if log file not empty
+`checkFileNotEmpty "./token.log"`
 file_empty_status=$?
 if [[ $file_empty_status != 0 ]]
 then 
 	echo "Please make sure to run the login script before making an auth request"
 	exit 1
 fi
-
 unset file_empty_status
 
+# verify refresh token
+`verifyToken "$refresh_token" "/MainApp/verify/"`
+verify_response=$?
+# check if verification passed. 
+if [[ $verify_response -eq 7 ]] # fatal error in the request.
+then 
+	echo "Fatal error in request!"
+	echo "Host is not up!"
+	exit 7
+elif [[ $verify_response -ne 0 ]] # an uknown error.
+then
+	echo "The refresh token is invalid!"
+	echo "Please re-login!"
+	exit 1
+fi
+unset verify_response
+
+# verify access toekn
+`verifyToken "$access_token" "/MainApp/verify/"`
+verify_response=$?
+# check if verification passed else refresh.
+if [[ $verify_response -ne 0 ]]
+then echo "Please refresh the token!";exit 1;
+fi
+
+# finally make the request.
 request_response=$(curl -s\
 	-H "Authorization: Bearer $access_token"\
 	--url $request_url)
