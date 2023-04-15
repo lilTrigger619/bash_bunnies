37d98117d7b33b63ea30d603ef0b91c94a2fa683
Shadrack Opoku <shaddyopoku1@gmail.com>
2023-04-15 03:33:24 +0000

    chore: Added the refresh script to refresh the access token.

---
+++ api_scripts/curl_refresh.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+: '
+This program is used to make a refresh request when the access token expires.
+But will return an error when the refresh token has expired or does not exist.
+Please not is uses the token stored in the token.log file in the source directory.
+'
+
+URL="http://localhost:8000/MainApp/refresh/"
+source "./utils.sh"
+
+`checkFileExist "./token.log"` # check if the token.log  file exists.
+
+`verifyToken "refresh" "/MainApp/verify/"` # verify if the refresh token exists.
+verify_status=$?
+
+# check verification status.
+if [[ $verify_status -eq 7 ]]
+then 
+	echo Fatal error occured
+	exit 7
+elif [[ $verify_status -eq 0 ]]
+then 
+	echo Refresh token not expired!
+	echo No need to refresh.
+	exit 0
+fi 
+
+# get the refresh token.
+refresh_token=$(getToken "./token.log" "refresh")
+
+# make refresh request otherwise
+refresh_response=$(curl -s -X POST -d refresh\=$refresh_token --url $URL)
+cmd_status=$?
+
+# checking the status of the curl command request.
+if [[ $cmd_status -eq 7 ]]
+then 
+	echo There was a fatal error!
+	echo The host may down!.
+	exit 7
+elif [[ $cmd_status -ne 0 ]]
+then
+	echo An unknown error occured!
+	exit $cmd_status
+fi
+unset cmd_status
+
+# when the request is successful.
+access_token=$(echo $refresh_response | grep -oP "(?<=\"access\":\")[^\"]+")
+cmd_status=$?
+
+if [[ $cmd_status -ne 0 ]]
+then echo Access token no found!
+	exit $cmd_status
+fi
+
+# when the grep cmd is successful
+echo $access_token
+
